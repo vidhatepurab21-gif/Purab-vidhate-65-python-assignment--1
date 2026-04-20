f3673f89d721c8f8607d0f5bafc9a98dd49d9c6e
Purab Vidhate <vidhatepurab21@gmail.com>
2026-04-21 01:51:01 +0530

    Add file writing functionality in Write_txt.py

---
+++ Write_txt.py
@@ -0,0 +1,5 @@
+file = open("sample.txt", "w")
+file.write("Hello, this is the first line in the file.\n")
+file.write("File handling in Python is easy to learn.\n")
+file.close() # Closing the file
+print("Data written successfully.\n")
